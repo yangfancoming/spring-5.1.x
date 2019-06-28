72f72de804d04a2e3c33b04191e290009e50024d
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-06-28 10:45:06 +0800

    增加  ClassPathXmlApplicationContext  demo 测试

---
+++ spring-demo/src/main/java/com/goat/spring/demo/service/MessageService.java
@@ -0,0 +1,12 @@
+package com.goat.spring.demo.service;
+
+/**
+ * Created by 64274 on 2019/6/28.
+ *
+ * @ Description: TODO
+ * @ author  山羊来了
+ * @ date 2019/6/28---10:39
+ */
+public interface MessageService {
+	String getMessage();
+}
