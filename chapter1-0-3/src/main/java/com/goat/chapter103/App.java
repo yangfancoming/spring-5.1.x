4e93fde6a068da284c492c9cdee0610666df6e00
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-03-16 11:49:20 +0800

    学习 spring 资源文件加载

--- chapter1-0-3/src/main/java/com/goat/chapter103/App.java
+++ chapter1-0-3/src/main/java/com/goat/chapter103/App.java
@@ -9,29 +9,24 @@ import java.io.IOException;
 
 /**
  * Created by 64274 on 2019/8/18.
- *
  * @ Description: Spring资源文件读取
  * @ author  山羊来了
  * @ date 2019/8/18---12:33
  */
 public class App {
 
-
+	// 从资源文件夹下加载
 	@Test
 	public void test1() {
-		// 从资源文件夹下加载
 		Resource resource = new ClassPathResource("application.xml");
 		print(resource);
-
 	}
 
 	@Test
 	public void test2() {
 		// 使用类信息加载   doit 为啥读取不到配置文件？？
-//		Resource resource = new ClassPathResource("application.xml", Person.class);
-		Resource resource = new ClassPathResource("bean.xml", App.class);
+		Resource resource = new ClassPathResource("application.xml", Person.class);
 		print(resource);
-
 	}
 
 	@Test
@@ -50,7 +45,7 @@ public class App {
 
 	// 打印资源文件内容
 	public void print(Resource resource) {
-		byte[] read = new byte[10000];
+		byte[] read = new byte[1000];
 		try {
 			resource.getInputStream().read(read, 0, read.length);
 			System.out.println(new String(read));
