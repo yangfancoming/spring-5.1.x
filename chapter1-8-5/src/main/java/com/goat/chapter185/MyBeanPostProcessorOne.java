d6dceaa25a0495a49a817b87531cfe94148cf815
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-03-26 08:46:13 +0800

    代码格式优化

--- chapter1-8-5/src/main/java/com/goat/chapter185/MyBeanPostProcessorOne.java
+++ chapter1-8-5/src/main/java/com/goat/chapter185/MyBeanPostProcessorOne.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.core.Ordered;
 
 public class MyBeanPostProcessorOne implements BeanPostProcessor, Ordered {
+
 	@Override
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 		System.out.println("BeanPostProcessor第" + getOrder() + "次被调动\n");
