c8f18025b5e50b7cd9e53b25023125c19b15cf1b
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-03-28 16:25:41 +0800

    增加     chapter1-0-2  xsd  （Xml Schemas Definition ）

---
+++ chapter1-0-2/src/main/java/com/goat/chapter102/App.java
@@ -0,0 +1,18 @@
+package com.goat.chapter102;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2020/3/28.
+ *
+ * @ Description: TODO
+ * @ author  山羊来了
+ * @ date 2020/3/28---16:24
+ */
+public class App {
+
+	@Test
+	public void test(){
+
+	}
+}
