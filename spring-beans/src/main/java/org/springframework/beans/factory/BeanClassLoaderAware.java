e06af492caa1c7ae80c08202c445bd22164203ae
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-04-21 13:40:51 +0800

    增加 BeanClassLoaderAware 测试用例

--- spring-beans/src/main/java/org/springframework/beans/factory/BeanClassLoaderAware.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/BeanClassLoaderAware.java
@@ -3,13 +3,11 @@
 package org.springframework.beans.factory;
 
 /**
- * Callback that allows a bean to be aware of the bean
- * {@link ClassLoader class loader}; that is, the class loader used by the
- * present bean factory to load bean classes.
+ * Callback that allows a bean to be aware of the bean  {@link ClassLoader class loader};
+ * that is, the class loader used by the present bean factory to load bean classes.
  *
  * This is mainly intended to be implemented by framework classes which
- * have to pick up application classes by name despite themselves potentially
- * being loaded from a shared class loader.
+ * have to pick up application classes by name despite themselves potentially being loaded from a shared class loader.
  * For a list of all bean lifecycle methods, see the {@link BeanFactory BeanFactory javadocs}.
  * @since 2.0
  * @see BeanNameAware
@@ -20,11 +18,9 @@ public interface BeanClassLoaderAware extends Aware {
 
 	/**
 	 * Callback that supplies the bean {@link ClassLoader class loader} to  a bean instance.
-	 * Invoked <i>after</i> the population of normal bean properties but
-	 * <i>before</i> an initialization callback such as
+	 * Invoked <i>after</i> the population of normal bean properties but <i>before</i> an initialization callback such as
 	 * {@link InitializingBean InitializingBean's}
-	 * {@link InitializingBean#afterPropertiesSet()}
-	 * method or a custom init-method.
+	 * {@link InitializingBean#afterPropertiesSet()}  method or a custom init-method.
 	 * @param classLoader the owning class loader
 	 */
 	void setBeanClassLoader(ClassLoader classLoader);
