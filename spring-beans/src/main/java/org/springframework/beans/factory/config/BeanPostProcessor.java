c253db680f1ce3b42d96522e27342a394d0836d5
yangfan <34465021+jwfl724168@users.noreply.github.com>
2021-06-13 19:03:23 +0800

    学习 BeanPostProcessor 接口

--- spring-beans/src/main/java/org/springframework/beans/factory/config/BeanPostProcessor.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/BeanPostProcessor.java
@@ -7,21 +7,24 @@ import org.springframework.lang.Nullable;
 
 /**
  * Factory hook that allows for custom modification of new bean instances, e.g. checking for marker interfaces or wrapping them with proxies.
- * 允许自定义修改新的bean实例的工厂钩子，例如检查标记接口或用代理包装它们。
- *
  * ApplicationContexts can autodetect BeanPostProcessor beans in their bean definitions and apply them to any beans subsequently created.
- * 应用上下文可以在bean定义中自动检测 后置处理的bean，并将其应用于随后创建的任何bean。
  * Plain bean factories allow for programmatic registration of post-processors,applying to all beans created through this factory.
- * 纯bean工厂允许对后处理器进行编程注册，应用于通过该工厂创建的所有bean。
- *
  * Typically, post-processors that populate beans via marker interfaces or the like will implement {@link #postProcessBeforeInitialization},
  * while post-processors that wrap beans with proxies will normally implement {@link #postProcessAfterInitialization}.
+ *
+ * 允许自定义修改新的bean实例的工厂钩子，例如检查标记接口或用代理包装它们。
+ * 应用上下文可以在bean定义中自动检测 后置处理的bean，并将其应用于随后创建的任何bean。
+ * 纯bean工厂允许对后处理器进行编程注册，应用于通过该工厂创建的所有bean。
  * @since 10.10.2003
  * @see InstantiationAwareBeanPostProcessor
  * @see DestructionAwareBeanPostProcessor
  * @see ConfigurableBeanFactory#addBeanPostProcessor
  * @see BeanFactoryPostProcessor
  * 用来 动态修改bean示例的属性  修改实例  修改成熟态
+ * sos 应用场景：
+ * 	1，可以解析bean中的一些注解转化为需要的属性
+ *  2，注入处理一些统一的属性，而不用在每个bean中注入
+ *  3，在Spring开发过程中，存在同一个接口有多个实现类的情况，根据不同的应用场景，通常在具体调用的地方来选择不同的接口实现类，Spring的BeanPostProcessor是个好的选择。
  */
 public interface BeanPostProcessor {
 
