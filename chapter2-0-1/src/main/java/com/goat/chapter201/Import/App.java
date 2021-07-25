02a969aaa6c135277acd122cd16b8b60dce31297
yangfan <34465021+jwfl724168@users.noreply.github.com>
2021-07-25 08:56:33 +0800

    代码格式优化

--- chapter2-0-1/src/main/java/com/goat/chapter201/Import/App.java
+++ chapter2-0-1/src/main/java/com/goat/chapter201/Import/App.java
@@ -14,8 +14,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * 		3）、ImportBeanDefinitionRegistrar:手动注册bean到容器中
  * 4）、使用Spring提供的 FactoryBean（工厂Bean）;
  * 		1）、默认获取到的是工厂bean调用getObject创建的对象
- * 		2）、要获取工厂Bean本身，我们需要给id前面加一个&
- * 			&colorFactoryBean
+ * 		2）、要获取工厂Bean本身，我们需要给id前面加一个&   例如：&colorFactoryBean
  */
 public class App {
 
--- chapter2-0-1/src/main/java/com/goat/chapter201/Import/MyDeferredImportSelector.java
+++ chapter2-0-1/src/main/java/com/goat/chapter201/Import/MyDeferredImportSelector.java
@@ -5,12 +5,10 @@ import org.springframework.core.type.AnnotationMetadata;
 
 /**
  * Created by Administrator on 2021/6/21.
- *
  * @ Description: TODO
  * @ author  山羊来了
  * @ date 2021/6/21---16:22
  */
-
 public class MyDeferredImportSelector implements DeferredImportSelector {
 	@Override
 	public String[] selectImports(AnnotationMetadata importingClassMetadata) {
